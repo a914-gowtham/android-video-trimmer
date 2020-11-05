923fac2d065c30e491a54b2dffde08010989e805
Gowtham <a915gowtham@gmail.com>
2020-11-05 10:46:38 +0530

    toolbar title softcoded

--- library/src/main/java/com/gowtham/library/ui/ActVideoTrimmer.java
+++ library/src/main/java/com/gowtham/library/ui/ActVideoTrimmer.java
@@ -118,7 +118,7 @@ public class ActVideoTrimmer extends AppCompatActivity {
         setContentView(R.layout.act_video_trimmer);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setUpToolBar(getSupportActionBar(), getString(R.string.txt_edt_txt));
+        setUpToolBar(getSupportActionBar(), getString(R.string.txt_edt_video));
         toolbar.setNavigationOnClickListener(v -> finish());
         progressView = new CustomProgressView(this);
     }
