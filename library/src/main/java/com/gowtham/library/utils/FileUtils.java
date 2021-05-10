8ed6e77552d85804d2297aaadfb909ecfea45278
hatt0519 <moroku0519@gmail.com>
2021-05-10 12:42:50 +0900

    inline

--- library/src/main/java/com/gowtham/library/utils/FileUtils.java
+++ library/src/main/java/com/gowtham/library/utils/FileUtils.java
@@ -21,10 +21,9 @@ public class FileUtils {
 
     @SuppressLint("NewApi")
     public static String getPath(Context context, final Uri uri) {
-        final boolean isKitKat = Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT;
         String selection = null;
         String[] selectionArgs = null;
-        if (isKitKat ) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
             if (isExternalStorageDocument(uri)) {
                 final String docId = DocumentsContract.getDocumentId(uri);
                 final String[] split = docId.split(":");
