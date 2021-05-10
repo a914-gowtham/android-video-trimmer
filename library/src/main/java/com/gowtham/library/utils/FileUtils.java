94b871d9df657972cfbfda09be0590596bc0d55a
Gowtham Balamurugan <68514286+a914-gowtham@users.noreply.github.com>
2021-05-10 18:29:13 +0530

    Update FileUtils.java

--- library/src/main/java/com/gowtham/library/utils/FileUtils.java
+++ library/src/main/java/com/gowtham/library/utils/FileUtils.java
@@ -21,9 +21,10 @@ public class FileUtils {
 
     @SuppressLint("NewApi")
     public static String getPath(Context context, final Uri uri) {
+        Boolean isKitkatOrAbove=Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
         String selection = null;
         String[] selectionArgs = null;
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+        if (isKitkatOrAbove) {
             if (isExternalStorageDocument(uri)) {
                 final String docId = DocumentsContract.getDocumentId(uri);
                 final String[] split = docId.split(":");
